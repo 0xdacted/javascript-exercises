3fbb1d7e6bb6b677859e76c6236593309bb5ab57
Red <crabmarket56@gmail.com>
2022-06-28 05:05:03 -0400

    Add functionality

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+   let finalString;
+    for (i = 1; i <= num; i++){
+   if (i === 1) {
+    finalString = string;
+   }
+   else {
+    finalString += string;
+   }
 };
-
+return finalString;
+}
+// this works as well -> return `${string.repeat(num)}`; 
 // Do not edit below this line
 module.exports = repeatString;
