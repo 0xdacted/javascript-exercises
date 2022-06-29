23d0312fa13c085de53518d74724e513698a3689
Red <crabmarket56@gmail.com>
2022-06-28 22:55:28 -0400

    Add functionality

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const array = args[0];
+   let newArray = [];
 
-};
-
+   array.forEach((item) => {
+    if(!args.includes(item)) {
+        newArray.push(item);
+    } 
+   });
+   return newArray;
+}
+// var removeFromArray = function(...args) {
+//   const array = args[0]
+//   return array.filter(val => !args.includes(val))
+// }
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(i, x) {
+    let sum = 0;
+    
+    if (i < 0 || x < 0)
+    {
+        return "ERROR"
+    }
 
+    if (!Number.isInteger(i) || !Number.isInteger(x)) {
+        return "ERROR"
+    }
+    
+    else if (x > i){
+    for (i; i <=x; i++) {
+        sum += i;
+    }}
+
+    else {
+        for (x; x <=i; x++) {
+            sum+= x;
+        } 
+    }
+    return sum;
 };
 
 // Do not edit below this line
