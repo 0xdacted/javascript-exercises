a06b2e34255b268a15cff9616193fd3fecc1c3cc
Red <crabmarket56@gmail.com>
2022-07-04 08:49:01 -0400

    Add palindrome checking functionality

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = (text) => {
+    let string = text.toString();
+    const filteredString = string.replace(/[\W_]/g, '').replace(/\s/g, '').toLowerCase();
+    
+    let word = '';
 
-};
+    for (let i = filteredString.length - 1; i >= 0; i--) {
+        word += filteredString[i];
+    }
+       if (word === filteredString) {
+        return true;
+       }
+       else {
+        return false;
+       }
+}; 
+
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes; 
