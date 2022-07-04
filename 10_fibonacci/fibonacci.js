235c83f066a4bf553b029cefa95b4d39db59fc18
Red <crabmarket56@gmail.com>
2022-07-04 10:19:54 -0400

    Add fibonacci functionality

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibArray = [];
+fibArray[0] = 0;
+fibArray[1] = 1;
 
+const fibonacci = (n) => {
+    if (n < 0) {
+        return "OOPS";
+    }
+    
+    for (i = 2; i <= n; i++) {
+        fibArray[i] = fibArray[i-1] + fibArray[i-2];
+    }
+    return fibArray[n];
 };
 
 // Do not edit below this line
