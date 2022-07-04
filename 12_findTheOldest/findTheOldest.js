3259aa4dd65698a6148566e9dd7d30ee006de55d
Red <crabmarket56@gmail.com>
2022-07-04 15:18:37 -0400

    Add functionality

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
 
+
+const findTheOldest = function(people) {
+    
+    people.sort(function(a,b) {
+    let lastPerson = a.yearOfDeath - a.yearOfBirth;
+    let nextPerson = b.yearOfDeath - b.yearOfBirth;
+    if (a.yearOfDeath === undefined) {
+        lastPerson = parseInt(new Date().getFullYear()) - a.yearOfBirth;
+    }
+    if (b.yearOfDeath === undefined){
+        nextPerson = parseInt(new Date().getFullYear()) - b.yearOfBirth;
+    }
+    
+    if (lastPerson > nextPerson) {
+      return -1;
+    } else {
+      return 1;
+    }
+}); 
+return people[0];
 };
 
 // Do not edit below this line
